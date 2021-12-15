1ff58bf54bb8271b2c531ed89198c0479cc08a6f
Kamil Lachowicz <kamil109111@gmail.com>
2021-12-16 00:57:55 +0100

    Wstrzykiwanie zależności i rejestracja serwisów

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IPostService, PostService>();
+            services.AddSingleton(AutoMapperConfig.Initialize());
+
+            return services;
+        }
+    }
+}
---
+++ WebAPI/Installers/MvcInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Microsoft.Extensions.Configuration;
+using Domain.Interfaces;
+using Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Application;
+using Infrastructure;
+
+namespace WebAPI.Installers
+{
+    public class MvcInstaller : IInstaller
+    {      
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddApplication();
+            services.AddInfrastructure();
+            services.AddControllers();            
+        }
+    }
+}
